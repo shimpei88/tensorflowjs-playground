46cfb1caff1cace2cc890221f973f29f0a52fcc9
shimpei88 <bigisland793@gmail.com>
2019-12-21 15:21:52 +0900

    inital commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const block = {
+    blockHeader: {
+        prevHash: '',
+        difficulty: '',
+        timeStamp: null,
+        nonce: {}
+    },
+    transaction: {
+
+    },
+
+    blockHash: {
+
+    }
+}
+
+console.log(block
